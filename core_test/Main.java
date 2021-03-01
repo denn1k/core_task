79759afae19caab48c31be8f7a1f427778e842b6
denn1k <dvbobnikhov@gmail.com>
2021-03-01 16:04:18 +0200

    Done all tasks (probably)

--- core_test/Main.java
+++ core_test/Main.java
@@ -1,59 +1,119 @@
 package core_test;
 
+import HW240221.People;
+
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
-import java.util.List;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, NoSuchSerialException {
 
+
+        Scanner scanner = new Scanner(System.in);
 
         try (InputStream inputStream = new FileInputStream("F:\\Task\\deviceInfo.txt");
 
         ) {
 
 
-            String str = ", ";
+            String str = "";
             while (inputStream.available() > 0) {
                 str += (char) inputStream.read();
             }
 
-            System.out.println(str);
-            String[] words = str.split("/n");
-            for (int i = 0, wordsLength = words.length; i < wordsLength; i++) {
-                String word = words[i];
-                System.out.println(word);
+            //System.out.println(str);
+            String[] words = str.split("\n");
+            //for (int i = 0, wordsLength = words.length; i < wordsLength; i++) {
+                //String word = words[i];
+                //System.out.println(word);
+            //}
+            List<String> ss = Files.lines(Path.of("F:\\Task\\deviceInfo.txt")).collect(Collectors.toList());
+            //System.out.println(ss);
+            List<Device> devices = new ArrayList<>();
+
+            for (int i = 0; i < ss.size(); i++) {
+
+                String[] parse = ss.get(i).split(", ");
+                devices.add(new Device(Long.parseLong(parse[0]), parse[1], parse[2], Integer.parseInt(parse[3])));
+                System.out.println(devices.get(i));
             }
-            Device device  = new Device(Long.parseLong(words[0]), words[1], words[2], Integer.parseInt(words[3]));
-    //Device device1 = new Device(Long.parseLong(words[4]),words[5], words[6], Integer.parseInt(words[7]));
-    //Device device2 = new Device(Long.parseLong(words[8]),words[9], words[10], Integer.parseInt(words[11]));
-    //Device device3 = new Device(Long.parseLong(words[12]),words[13], words[14], Integer.parseInt(words[15]));
+
+            List<Device> sortedDevices = devices.stream()
+                    .filter(elemen -> elemen.getPrice() > 100 && elemen.getPrice() <= 800 && "Apple".equals(elemen.getBrand()))
+                    .collect(Collectors.toList());
+            //System.out.println("Sorted : " + sortedDevices);
 
 
-            //byte[] bytesArr = device.toString().getBytes(StandardCharsets.UTF_8);
 
-            ///List<Device> devices = Arrays.asList(device,device1,device2,device3);
-
-            //System.out.println("Devices : " + devices);
             System.out.println("___________");
+            while (true) {
+                System.out.println("" +
+                        "\n1 - Вивести весь список на екран." +
+                        "\n2 - Шукати Device за серійним номером " +
+                        "\n3 - Вивести посортований список за назвою бренду." +
+                        "\n4 - Вивести лише ті Device у яких ціна більша ніж 400." +
+                        "\n5 - Вивести лише ті Device у яких в серійному номері є нуль(0) і ціна менша за 800." +
+                        "\n6 - Вивести найдорожчий товар." +
+                        "\n0 - Вихід з програми.");
+                String select = scanner.next();
+                switch (select) {
+                    case "1": {
+                        devices.forEach(System.out::println);
+                        break;
+                    }
+                    case "2": {
+                        System.out.println("Ведіть серійний номер");
+                        long serial =scanner.nextLong();
+                        List<Device> snumbersList = devices.stream()
+                                .filter(elemen ->elemen.getSerialNumber()==serial)
+                                .collect(Collectors.toList());
+                        System.out.println(snumbersList);
 
-            //1. Вивести весь список на екран.
+                        break;
+                    }
+                    case "3": {
+                        List<Device> sortedByBrand = devices.stream()
+                                .sorted(Comparator.comparing(Device::getBrand))
+                                        .collect(Collectors.toList());
+                        System.out.println("Sorted by brand" + sortedByBrand);
+                        break;
+                    }
+                    case "4": {
+                        List<Device> moreThen = devices.stream()
+                                .filter(elemen -> elemen.getPrice() > 400 )
+                                .collect(Collectors.toList());
+                        System.out.println("More then 400 USD : " + moreThen);
 
-            //2. Шукати Device за серійним номером (Якщо такого Device немає кидати помилку, яку створите самостійно).
+                        break;
+                    }
+                    case "5": {
+                        List<Device> zeroPlusPrice800 = devices.stream()
+                                .filter(elemen -> String.valueOf(elemen.getSerialNumber()).contains("0"))
+                                .collect(Collectors.toList());
+                        System.out.println("Less then 800 USD : " + zeroPlusPrice800);
 
-            //3. Вивести посортований список за назвою бренду.
+                        break;
+                    }
+                    case "6": {
+                        List<Device> theMostExpensive = devices.stream().max(Comparator.comparing(Device::getPrice)).stream().collect(Collectors.toList());
+                        System.out.println("The most expensive device : " + theMostExpensive);
 
-            //4. Вивести лише ті Device у яких ціна більша ніж 400.
+                        break;
+                    }
 
-            //5. Вивести лише ті Device у яких в серійному номері є нуль(0) і ціна менша за 800.
+                    default: {
+                        System.exit(0);
+                    }
 
-            //6. Вивести найдорожчий товар.
+                }
+            }
 
-        } catch (IOException e) {
-            e.printStackTrace();
-
-        }
+            }
 
     }
-}
+    }
